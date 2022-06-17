f455ec1fb1a677e19496fb53b0f486b3a843760f
Mihail Sorokin <85826837+mihail228350df@users.noreply.github.com>
2022-06-17 19:38:51 +0300

    scss edit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
 function App() {
   return ( 
-    <div className="wrapper">
-      <header>
-        <div className="headerLeft">
+    <div className="wrapper clear">
+      <header className="d-flex justify-between align-center p-40">
+        <div className="d-flex align-centr">
          <img width={40} height={40} src= "/img/logo.png" />
          <div className="headerInfo">
-           <h3>React Sneakers</h3>
+           <h3 className="text-uppercase">React Sneakers</h3>
            <p>Магазин лутших кросовок</p>
          </div>
         </div>
         
-        <ul className="headerRight">
-          <li>
-            <svg/> 
+        <ul className="d-flex">
+          <li className="mr-30">
+          <img width={18} height={18} src= "/img/basket.svg" /> 
             <span>1205 грн.</span>
           </li>
           <li>
-            <svg/>
+          <img width={40} height={40} src= "/img/user.svg" />
           </li>
         </ul>
       </header>
-      <div className="content">
-        <h1>Все кросовки</h1>
+      <div className="content p-40">
+        <h1>Все кросcовки</h1>
         ...
       </div>
     </div>
